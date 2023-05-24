6911b5f3d7b4b208f4e9932fd8d7fab99a2c1143
Rory Kane <roarigami@gmail.com>
2023-05-24 12:13:15 -0400

    More languages

--- csesPsetIntro/c++/algo1.cpp
+++ csesPsetIntro/c++/algo1.cpp
@@ -1,4 +1,4 @@
-/**
+/*
 CSES Problem #1 Weird algorithm
 Question - Consider an algorithm that takes as
 input a positive integer n. If n is even, the algorithm
@@ -6,16 +6,24 @@ divides it by two, and if n is odd, the algorithm multiplies
 it by three and adds one. The algorithm repeats this, until n is one.
 For example, the sequence for n = 3 is as follows
 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
-**/
+*/
 
-const addSubToOne = (num) => {
-    while (num > 1) {
-        if(num % 2 == 0)
-            num /= 2;
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+
+    ll n;
+    cin >> n;
+    cout << n;
+    while(n > 1) {
+        if(n % 2 == 0)
+            n /= 2;
         else
-            num = num * 3 + 1;
-        console.log(num);
-  }
-  return num;
+            n = 3 * n + 1;
+        cout << " " << n;
+        
+    }
 }
-console.log(addSubToOne(47));
