13b21cb25cc860ac05af80e3570179c3ebe932ad
Rory Kane <roarigami@gmail.com>
2023-06-08 14:21:46 -0400

    Intro problem #2

--- csesPsetIntro/python/algo2.py
+++ csesPsetIntro/python/algo2.py
@@ -3,3 +3,6 @@ CSES Problem Set Intro #2 Missing Number
 You are given all numbers between 1,2,…,n except one.
 Your task is to find the missing number.
 """
+n = int(input())
+ans = n * (n+1)//2 - sum(int(i) for i in input().split())
+print(ans)
