c5b26626fadff40452855f10b0d0b3c5baac172e
Rory Kane <roarigami@gmail.com>
2023-06-08 16:35:59 -0400

    edits

--- csesPsetIntro/python/algo2.py
+++ csesPsetIntro/python/algo2.py
@@ -4,5 +4,9 @@ You are given all numbers between 1,2,…,n except one.
 Your task is to find the missing number.
 """
 n = int(input())
-ans = n * (n+1)//2 - sum(int(i) for i in input().split())
-print(ans)
+
+def missingNum(n):
+    ans = n * (n+1)//2 - sum(int(i) for i in input().split())
+    return ans
+
+print(missingNum(n))
